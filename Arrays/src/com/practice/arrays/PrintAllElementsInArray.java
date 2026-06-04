075895fa3377854d46de0f2a350549fb45ea51c2
Kishore060 <yerramsettikishore060@gmail.com>
2026-06-04 22:21:08 +0530

    Array Problems

---
+++ Arrays/src/com/practice/arrays/PrintAllElementsInArray.java
@@ -0,0 +1,14 @@
+package com.practice.arrays;
+
+public class PrintAllElementsInArray {
+
+	public static void main(String[] args) {
+		
+		int[] input = {1,3,4,5,6,7,7};
+		for(int i=0;i<input.length;i++) {
+			System.out.print(input[i]+" ");
+		}
+
+	}
+
+}
